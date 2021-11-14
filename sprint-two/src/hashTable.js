0766bfa6aa184253ec9f240874286bb72d730b83
Daniel-Ghaly <ghalydan@gmail.com>
2021-11-13 16:14:12 -0800

    Passed all of hasTable except extra credit

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,6 +2,7 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this._count = 0;
   this._storage = LimitedArray(this._limit);
 };
 
@@ -9,20 +10,84 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  var bucket = this._storage.get(index);
+  if ( !bucket ) {
+    bucket = [];
+    this._storage.set(index, bucket);
+  }
+
+  var found = false;
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      tuple[1] = v;
+      found = true;
+    }
+  }
+
+  if (found === false) {
+    bucket.push([k, v]);
+    this._count++;
+    if (this.count > this._limit * 0.75) {
+      this._resize(this._limit * 2);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var bucket = this._storage.get(index);
+  if (!bucket) {
+    return undefined;
+  }
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
+
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (!bucket) {
+    return undefined;
+  }
 
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+      this._count--;
+      if (this.count < this._limit * 0.25) {
+        this._resize(this._limit / 2);
+      }
+      return tuple[1];
+    }
+  }
+  return undefined;
 };
 
+HashTable.prototype._resize = function(newLimit) {
+  var old = this._storage;
+  this._limit = newLimit;
+  this._count = 0;
+  this._storage = LimitedArray(this._limit);
 
+  old.each(function(bucket) {
+    if (!bucket) {
+      return;
+    }
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1]);
+    }
+  });
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
